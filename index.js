a6d85abb7325552490f77f3c018d83fb97e8fd31
Jessie He <jessie.he@deakin.edu.au>
2023-11-01 11:13:54 +1100

    extract blogs router to a controller file

--- index.js
+++ index.js
@@ -4,28 +4,11 @@ const logger = require('./utils/logger')
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const Blog = require('./models/blogs')
+const blogsRouter = require('./controllers/blogs')
 
 app.use(cors())
 app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+app.use('/api/blogs', blogsRouter) //router is a middleware
 
 app.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`)
