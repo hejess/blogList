5a957861e38c6fecd787ff392c304cc617852b2d
Jessie He <jessie.he@deakin.edu.au>
2023-11-01 16:19:52 +1100

    testing with jest, 4.5

---
+++ utils/list_helper.js
@@ -0,0 +1,19 @@
+const dummy = (blogs) => 1
+
+const totalLikes = (blogs) => {
+    return blogs.reduce(
+        (sum, blog) => sum + blog.likes, 0)
+}
+
+const favoriteBlog = (blogs) => {
+    if (blogs.length === 0) {
+        return null
+    }
+    const result = blogs.reduce((favorite, blog)=> blog.likes > favorite.likes ? blog : favorite, blogs[0])
+    // Create a new object by destructuring the original object and excluding the specified properties
+    const { __v, _id, url, ...newObject } = result;
+    return newObject
+}
+module.exports = {
+    dummy, totalLikes, favoriteBlog
+}
